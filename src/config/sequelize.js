08c1ae4157bcf36c4b187127e24fc1178023dfb5
John Hawley <jehawley@gmail.com>
2025-07-25 16:55:30 -0700

    14

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
 require('dotenv').config();
+require('pg');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../../config/database.js')[env];

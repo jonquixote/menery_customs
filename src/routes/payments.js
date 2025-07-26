c6915f771bbd3e60b093168bbc3f4e025ad5ccca
John Hawley <jehawley@gmail.com>
2025-07-25 17:15:51 -0700

    16

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const PaymentController = require('../controllers/paymentController');
 
-// POST /api/payments/create-payment-link - Create a payment link (Square or PayPal)
-router.post('/create-payment-link', PaymentController.createPaymentLink);
-
 // GET /api/payments/status/:paymentId/:paymentMethod - Get payment status
 // Note: paymentMethod needs to be passed to the controller to determine which service to use.
 router.get('/status/:paymentId/:paymentMethod', PaymentController.getPaymentStatus);
